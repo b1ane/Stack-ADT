f27ba83eeb88eb1ed5d6b6f111a82256708067ae
Blane Zewdie <b1ane@outlook.com>
2022-09-14 17:38:06 -0500

    Initial Commit

---
+++ Stack ADT/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Stack ADT
+//
+//  Created by blane on 9/14/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Stack ADT/stack.h
@@ -0,0 +1,12 @@
+//
+//  stack.h
+//  Stack ADT
+//
+//  Created by blane on 9/14/22.
+//
+
+#ifndef stack_h
+#define stack_h
+
+
+#endif /* stack_h */
