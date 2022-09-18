58119953ba508204c01b21966ba0cb19e8202134
Blane Zewdie <b1ane@outlook.com>
2022-09-17 23:35:52 -0500

    infixtoprefix() function created -- not functional yet

--- Stack ADT/main.cpp
+++ Stack ADT/main.cpp
@@ -11,16 +11,6 @@ using namespace std;
 #include <sstream>
 #include "stack.h"
 
-//checks if charcter in string is digit
-bool isOperand(char o) {
-    if( o == '0' || o == '1' ||o == '2'|| o == '3'|| o == '4'|| o == '5'|| o == '6'|| o == '7'|| o == '8'|| o == '9') {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
 
 
 int main() {
@@ -30,29 +20,24 @@ int main() {
     string input;
     getline(cin, input);
     
-    stack digits; //holds operands
-    stack ops;    //holds operators
+    stack prefix;
+    stack output;
     
-    
-    //loops through entire equation
+    //loops through entire equation, pushes all values to prefix stack
     for( int i  = 0; i < input.size(); i++) {
-        //if value in string is a number -- push to digits
-        if( input.at(i) == '0' || input.at(i) == '1' ||input.at(i) == '2'||input.at(i) == '3'||input.at(i) == '4'||input.at(i) == '5'||input.at(i) == '6'||input.at(i) == '7'||input.at(i) == '8'||input.at(i) == '9') {
-            digits.push(input.at(i));
-        }
-        else { //push to ops
-            ops.push(input.at(i));
-        }
+        prefix.push(input.at(i));
     }
-     
     
-    
-    digits.print();
+    prefix.print(); //outputs entire prefix equation
     cout << endl;
     
-    ops.print();
+    stack st;
+    
+    output.infixTprefix(prefix,output,st);
+    output.print();
     cout << endl;
-
+    st.print();
+    cout << endl;
     
     
     
--- Stack ADT/stack.h
+++ Stack ADT/stack.h
@@ -18,16 +18,16 @@ struct node {
 
 class stack {
 private:
-    node* tp; //points to first element in stack
+    node* top; //points to first element in stack
 
 public:
     stack() { //constructor
-        tp = nullptr;
+        top = nullptr;
     }
     
     //returns value at front of list
-    void top() {
-        cout<< tp->data << endl;
+    char Top() {
+        return  top->data;
     }
     
     
@@ -37,20 +37,24 @@ public:
         node* newNode = new node;
         newNode->data = val;
         //top is to value passed
-        newNode->next = tp;
-        tp = newNode;
+        newNode->next = top;
+        top = newNode;
     }
     
     //deletes value at front of list
     void pop() {
         node* temp = new node;
-        temp = tp; //equal to first val
-        tp = tp->next;
+        temp = top; //equal to first val
+        top = top->next;
         delete temp;
     }
     
+    bool isEmpty() {
+        return top;
+    }
+    
     void print() {
-        node* cu = tp;
+        node* cu = top;
         
         while(cu!=nullptr) {
             cout << cu->data;
@@ -58,11 +62,65 @@ public:
         }
     }
     
+    //checks if charcter in string is digit
+    bool isOperand(char o) {
+        if( o == '0' || o == '1' ||o == '2'|| o == '3'|| o == '4'|| o == '5'|| o == '6'|| o == '7'|| o == '8'|| o == '9') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    //checks precedence for operators
+    int Hprecedence(char o) {
+        //if op is mult/div, has higher precedence
+        if( o == '*' || o == '/' ) {
+            return 2;
+        }
+        if ( o == '+' || o == '-') {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
     
     
     
-    
-    
+    void infixTprefix(stack p, stack &o, stack &st) {
+        //new node created set equal to top node
+        node* t = p.top;
+        
+        //scanning
+        while(t!=nullptr) {
+            //if it is a digit -- push to output
+            if (isOperand(t->data)) {
+                o.push(t->data);
+            }
+            //if it is NOT digit
+            if (!isOperand(t->data)){
+                //if stack is empty, push to stack
+                if(!st.isEmpty()) {
+                    st.push(t->data);
+                }
+                //if operator has a higher precedence, push to stack
+                //compare operator to stack's top operator
+                if ( Hprecedence(t->data) > Hprecedence(st.Top()) ) {
+                    st.push(t->data);
+                }
+                if ( Hprecedence(t->data) < Hprecedence(st.Top())) {
+                    //push operator at top of stack to output
+                    o.push(st.Top());
+                    //pop operator
+                    st.pop();
+                    //push to stack
+                    st.push(t->data);
+                }
+            }
+            t = t->next;
+        }
+    }
 };
 
 #endif /* stack_h */
